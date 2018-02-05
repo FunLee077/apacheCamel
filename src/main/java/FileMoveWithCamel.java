4a8d17ec7ae2fd529d419f1d24407e551bdcb835
hy <mrlee077@163.com>
2018-02-05 12:48:59 +0800

    测试1

---
+++ src/main/java/FileMoveWithCamel.java
@@ -0,0 +1,24 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * Created by hy on 2018/2/5.
+ */
+public class FileMoveWithCamel {
+    public static void main(String []arg) throws Exception{
+        CamelContext context=new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:F:/history").to("file:F:/home");
+            }
+        });
+        context.start();
+
+            Thread.sleep(5000);
+
+        context.stop();
+
+    }
+}
